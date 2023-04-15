47bedc5c3c18eac290bdf7d0a2ec99e141864ac4
Eduardo Coutinho <educoutinho@gmail.com>
2023-04-15 15:09:48 -0300

    add Delete OrdemServico method

--- src/OmieSharp/OmieSharpClient.cs
+++ src/OmieSharp/OmieSharpClient.cs
@@ -519,6 +519,42 @@ namespace OmieSharp
             }
         }
 
+        public async Task<OrdemServicoStatus> ExcluirOrdemServicoAsync(OrdemServicoChave chave)
+        {
+            var relativeUrl = new Uri("/api/v1/servicos/os/", UriKind.Relative);
+            var fullUrl = new Uri(baseUrl, relativeUrl);
+            var omieRequest = new OmieBaseRequest<OrdemServicoChave>("ExcluirOS", AppKey, AppSecret, chave);
+            var jsonRequest = JsonSerializer.Serialize(omieRequest, _jsonSerializerOptions);
+
+            try
+            {
+                var requestContent = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+                requestContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                var response = await this.HttpClient.PostAsync(fullUrl, requestContent);
+
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                if (!response.IsSuccessStatusCode)
+                {
+                    var errorResponse = JsonSerializer.Deserialize<ErrorResponse>(jsonResponse);
+
+                    //{"faultstring":"ERROR: Esta requisi\u00e7\u00e3o j\u00e1 foi processada ou est\u00e1 sendo processada e voc\u00ea pode tentar novamente \u00e0s 19:04:08. (1)","faultcode":"SOAP-ENV:Client-1100"}
+                    if ((errorResponse?.ErrorMessage ?? "").Contains("já foi processada"))
+                        throw new OmieSharpDuplicateRequestException();
+
+                    throw new OmieSharpWebException(response.StatusCode, $"Error: {errorResponse?.ErrorCode} {errorResponse?.ErrorMessage} (API StatusCode: {(int)response.StatusCode})", jsonRequest, jsonResponse);
+                }
+
+                var model = JsonSerializer.Deserialize<OrdemServicoStatus>(jsonResponse)!;
+
+                return model;
+            }
+            catch (Exception ex)
+            {
+                throw new OmieSharpException($"Error in Omie API Call {relativeUrl} -- {ex.Message} -- Url: {fullUrl}", ex);
+            }
+        }
+
         #endregion
 
         #region ContaCorrente
